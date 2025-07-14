ba56eba57834664b71a1ddd678803e07a7980e1e
Maria Sorokina <ruafelianna@ya.ru>
2025-07-14 15:17:19 +0300

    Add _errors module

---
+++ stubs/pyudev/__init__.py
@@ -0,0 +1,17 @@
+from ._errors import (
+    DeviceNotFoundAtPathError,
+    DeviceNotFoundByFileError,
+    DeviceNotFoundByNameError,
+    DeviceNotFoundByNumberError,
+    DeviceNotFoundError,
+    DeviceNotFoundInEnvironmentError,
+)
+
+__all__ = [
+    "DeviceNotFoundAtPathError",
+    "DeviceNotFoundByFileError",
+    "DeviceNotFoundByNameError",
+    "DeviceNotFoundByNumberError",
+    "DeviceNotFoundError",
+    "DeviceNotFoundInEnvironmentError",
+]
